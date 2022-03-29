72ef9d98493675bf98101034f4dcd2345dea8ba4
Anaelle Danton <anaelle.danton@gmail.com>
2022-03-29 22:49:18 +0200

    test heroku

--- src/wikishin/back/index.js
+++ src/wikishin/back/index.js
@@ -37,3 +37,13 @@ app.use((req, res, next) => {
     );
     next()
 });
+
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(dirname, '../front-cli/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dirname + '../front-cli/build/index.html'))
+})
